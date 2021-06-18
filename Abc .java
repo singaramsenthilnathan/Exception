b3846c446f89b04a9164791bacafe3aba9a58cb1
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-06-18 21:56:26 +0530

    MultiThreading

---
+++ Abc .java
@@ -0,0 +1,21 @@
+public class Abc 
+{
+public static void main(String[] args)
+{
+	Abc abc = new Abc();
+	abc.start();
+	
+
+}
+
+public void start()
+{
+	run();
+}
+
+public void run() 
+{
+	System.out.println("ABC");
+}
+
+}
