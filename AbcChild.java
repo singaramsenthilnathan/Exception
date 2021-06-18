cc2da441391b240db5e758c7e1f06aa8178f30be
singaramsenthilnathan <81078197+singaramsenthilnathan@users.noreply.github.com>
2021-06-18 21:57:06 +0530

    MultiThreading

---
+++ AbcChild.java
@@ -0,0 +1,15 @@
+public class AbcChild extends Abc
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+AbcChild abcChild = new AbcChild();
+abcChild.start();
+
+	}
+	public void run()
+	{
+		System.out.println("Child of ABC");
+	}
+
+}
